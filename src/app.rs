a8e54f0ac54053b57e6ac829475fa8f35677ac22
Christof Weickhardt <christof@weickhardt.ch>
2021-12-04 20:03:37 +0000

    feat: basic page setup

--- src/app.rs
+++ src/app.rs
@@ -32,8 +32,7 @@ struct Entry {
     editing: bool,
 }
 
-pub enum Msg {
-}
+pub enum Msg {}
 
 impl Component for App {
     type Message = Msg;
@@ -73,13 +72,152 @@ impl Component for App {
     fn view(&self) -> Html {
         info!("rendered!");
         html! {
-            <img src="./assets/images/cpp.png" />
+        <header>
+            <div
+                class="page-header min-vh-100"
+                style="background-image: url(./assets/images/background.svg)"
+                loading="lazy"
+            >
+                <span class="mask"></span>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-6 offset-lg-2">
+                            <div class="card" data-animation="true">
+                                <div class="card-body">
+                                    <section
+                                        class="py-9"
+                                        style="
+                                    padding: 16px !important;
+                                    padding-bottom: 0 !important;
+                                    padding-top: 16px !important;
+                                    "
+                                    >
+                                        <div class="container">
+                                            <div class="row">
+                                                <div class="col-lg-12 my-auto">
+                                                    <h3>{"Do you need to highlight some code?"}</h3>
+                                                        <p class="pe-4">
+                                                            {"This small webapp uses the"}
+                                                            <a
+                                                            href="https://code.google.com/archive/p/syntaxhighlighter/"
+                                                            >{"syntaxhighlighter"}</a
+                                                            >
+                                                            {"from Google Code Archive. Maybe you have seen it on"}
+                                                            <a href="http://planetb.ca/syntax-highlight-word"
+                                                            >{"planetb.ca"}</a
+                                                            >{". Sadly the planetb one is most of the times just
+                                                            not available. So have a go and see what you get."}
+
+                                                            <br />
+                                                            <br />
+                                                            {"All the code is held inside your browser, nothing
+                                                        leaves it. If you still worry and would like to host
+                                                        it yourselves, have a look at the docker part in"}
+                                                            <a
+                                                            href="https://github.com/somehowchris/plantetb-syntax-highlighter#docker"
+                                                            >{"this github repository"}</a
+                                                            >
+                                                            <br />
+                                                            <br />
+                                                        </p>
+
+                                                        <div class="row">
+                                                            <div class="col-md-12">
+                                                                {"It supports the following languages:"}
+                                                                <div class="row">
+                                                                    <div
+                                                                    class="col-sm-6 col-md-6 col-lg-4 col-xl-3"
+                                                                    >
+                                                                        <li>{"C++"}</li>
+                                                                    </div>
+                                                                    <div
+                                                                    class="col-sm-6 col-md-6 col-lg-4 col-xl-3"
+                                                                    >
+                                                                        <li>{"C#"}</li>
+                                                                    </div>
+                                                                    <div
+                                                                    class="col-sm-6 col-md-6 col-lg-4 col-xl-3"
+                                                                    >
+                                                                        <li>{"Python"}</li>
+                                                                    </div>
+                                                                    <div
+                                                                    class="col-sm-6 col-md-6 col-lg-4 col-xl-3"
+                                                                    >
+                                                                        <li>{"CSS"}</li>
+                                                                    </div>
+                                                                    <div
+                                                                    class="col-sm-6 col-md-6 col-lg-4 col-xl-3"
+                                                                    >
+                                                                        <li>{"Delphi"}</li>
+                                                                    </div>
+                                                                    <div
+                                                                    class="col-sm-6 col-md-6 col-lg-4 col-xl-3"
+                                                                    >
+                                                                        <li>{"VB"}</li>
+                                                                    </div>
+                                                                    <div
+                                                                    class="col-sm-6 col-md-6 col-lg-4 col-xl-3"
+                                                                    >
+                                                                        <li>{"Java"}</li>
+                                                                    </div>
+                                                                    <div
+                                                                    class="col-sm-6 col-md-6 col-lg-4 col-xl-3"
+                                                                    >
+                                                                        <li>{"JavaScript"}</li>
+                                                                    </div>
+                                                                    <div
+                                                                    class="col-sm-6 col-md-6 col-lg-4 col-xl-3"
+                                                                    >
+                                                                        <li>{"Ruby"}</li>
+                                                                    </div>
+                                                                    <div
+                                                                    class="col-sm-6 col-md-6 col-lg-4 col-xl-3"
+                                                                    >
+                                                                        <li>{"Sql"}</li>
+                                                                    </div>
+                                                                    <div
+                                                                    class="col-sm-6 col-md-6 col-lg-4 col-xl-3"
+                                                                    >
+                                                                        <li>{"XML/HTML"}</li>
+                                                                    </div>
+                                                                    <div
+                                                                    class="col-sm-6 col-md-6 col-lg-4 col-xl-3"
+                                                                    >
+                                                                        <li>{"PHP"}</li>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="row">
+                                                            <div class="col-12">
+                                                            <br />
+                                                            <br />
+                                                            <button
+                                                                type="button"
+                                                                class="btn btn-outline-success"
+                                                            >
+                                                                {"Don't show me this again"}
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </section>
+                                </div>
+                            </div>
+                            <br />
+                            <br />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
         }
     }
 }
 
-impl App {
-}
+impl App {}
 
 #[derive(EnumIter, ToString, Clone, PartialEq, Serialize, Deserialize)]
 pub enum Filter {
@@ -88,8 +226,6 @@ pub enum Filter {
     Completed,
 }
 
-impl Filter {
-}
+impl Filter {}
 
-impl State {
-}
+impl State {}
